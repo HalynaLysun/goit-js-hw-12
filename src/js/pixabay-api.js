9cf48c931a585820876e82beec4244549c3c72c1
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-02-28 23:15:31 +0200

    async/await

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,23 @@
-export async function searchImages(value, page) {
-  const URI = 'https://pixabay.com/api/';
-  const PARAMS = new URLSearchParams({
-    key: '42515030-f0931f035bd772c998b8c15c1',
-    q: value,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-  const LINK = `${URI}?${PARAMS}`;
+import axios from 'axios';
 
-  const response = await fetch(LINK);
+export async function searchImages(value) {
+  const URI = 'https://pixabay.com/api/';
 
   try {
-    !response.ok;
+    const response = await axios.get(URI, {
+      params: {
+        key: '42515030-f0931f035bd772c998b8c15c1',
+        q: value,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+    if (response.status !== 200) {
+      throw new Error('Error!');
+    }
+    return response.data;
   } catch {
     throw new Error('Error!');
   }
-
-  return await response.json();
 }
